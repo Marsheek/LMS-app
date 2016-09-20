01420ea8cfd775f25357a86d21ae115d04a42bc9
Marshall Gray <marshallgray1992@gmail.com>
2016-09-20 15:06:24 +1000

    add username to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+# this has been added because we altered devise and added username
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update) do |user|
+      user.permit(:username, :password, :password_confirmation, :current_password)
+    end
+  end
 end
