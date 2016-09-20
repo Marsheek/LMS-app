dbaab9833fe9423244eb4db3177f2d733c4632e7
Marshall Gray <marshallgray1992@gmail.com>
2016-09-20 15:21:34 +1000

    Books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920045150) do
+ActiveRecord::Schema.define(version: 20160920050735) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "author"
+    t.string   "title"
+    t.text     "description"
+    t.string   "book_category"
+    t.integer  "user_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
